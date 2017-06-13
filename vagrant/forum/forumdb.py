f53ad0d8f52dd7643d4caa00591243e0791f44a6
Eugene Ng <eugenen@utexas.edu>
2017-06-13 14:50:02 -0500

    used bleach to sanitize possible script inputs

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,6 +1,6 @@
 # "Database code" for the DB Forum.
 
-import datetime, psycopg2
+import datetime, psycopg2, bleach
 
 conn = psycopg2.connect("dbname=forum")
 cur = conn.cursor()
@@ -8,11 +8,11 @@ cur = conn.cursor()
 def get_posts():
   """Return all posts from the 'database', most recent first."""
   cur.execute("select * from posts order by time desc;")
-  return ((content, time) for (content, time, post_id) in cur.fetchall())
+  return ((bleach.clean(content), time) for (content, time, post_id) in cur.fetchall())
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  cur.execute("insert into posts (content) values (%s);", (content,))
+  cur.execute("insert into posts (content) values (%s);", (bleach.clean(content),))
   conn.commit()
 
 # cur.close()
